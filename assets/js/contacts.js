a0b30ad59142c9fbcda6af29ff290da2142eccd6
ProgKenhy <sashok20053@gmail.com>
2024-10-22 00:23:48 +0200

    second version

---
+++ assets/js/contacts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+	e.preventDefault();
+	alert('Спасибо, что связались с нами!');
+	// Позже здесь можно добавить логику для отправки формы
+});
